2d03cefbf90ef9ad3b0d6667169dfc603a7c2d46
Muhamad Akmal <akmalsheikh9918@gmail.com>
2024-11-27 11:35:17 +0530

    Ininial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      'x-rapidapi-key': '4102abd028mshdbfb543924bb13dp19a751jsn1b354dca818f',
+		  'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "166cf210b863b64e090791ab681e9811"; // enter your key from openweather API
